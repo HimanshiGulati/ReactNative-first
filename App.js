2297e7288254e3dcc8170f46a1514e4a8b5b9b04
himanshigulati <himanshigulati07@gmail.com>
2020-05-18 07:00:11 +0530

    React native first app basics

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  return <ViewImageScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
